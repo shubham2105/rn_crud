c9030dc6ebb100b740e7ead56b6c46d17a2ff2e5
shubham2105 <shubhamdhole215@gmail.com>
2026-01-14 23:23:55 +0530

    Gen Ai App development

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: "white" }}>App</Text>
+      <Text style={{ color: "white" }}>RN-GEN-AI</Text>
     </View>
   )
 }
